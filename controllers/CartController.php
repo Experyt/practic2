c42a677edc6538108b8aa72bdcad0232dd47dc5f
Experyt <kf_kf_95@mail.ru>
2019-01-21 17:34:35 +0300

    prac1

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -10,10 +10,56 @@ namespace app\controllers;
 use app\models\Good;
 use app\models\Cart;
 use Yii;
+use app\models\Order;
+use app\models\OrderGood;
 use yii\web\Controller;
 
 class CartController extends Controller
 {
+
+    public function actionOrder(){
+        $session = Yii::$app->session;
+        $session->open();
+        $order = new Order();
+        if($order->load(Yii::$app->request->post())){
+            $order->date = date('Y-m-d h:i:s');
+            $order->sum = $session['cart.totalSum'];
+            if ($order->save()){
+                Yii::$app->mailer->compose()
+                    ->setFrom(['aaa@mail.ru'=> 'test test'])
+                    ->setTo('asfsa@mail.ru')
+                    ->setSubject('Ваш заказ принят')
+                    ->send();
+                $session->remove('cart');
+                $session->remove('cart.totalQuantity');
+                $session->remove('cart.totalSum');
+                return $this->render('success', compact('session'));
+            }
+        }
+        $this->layout = 'empty-layout';
+        return $this->render('order', compact('session', 'order'));
+
+    }
+
+    public function actionDelete($id){
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->recalcCart($id);
+        return $this->renderPartial('cart', compact('session'));
+
+    }
+
+    public function actionClear(){
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.totalQuantity');
+        $session->remove('cart.totalSum');
+        return $this->renderPartial('cart', compact('session'));
+
+    }
+
 
     public function actionOpen(){
         $session = Yii::$app->session;
--- views/cart/cart.php
+++ views/cart/cart.php
@@ -28,7 +28,7 @@ if ($session['cart']) {
         <td style="vertical-align: middle"><?=$good['name']?></td>
         <td style="vertical-align: middle"><?=$good['goodQuantity']?></td>
         <td style="vertical-align: middle"><?=$good['price']*$good['goodQuantity']?> рублей</td>
-        <td class="delete" style="text-align: center; cursor: pointer; vertical-align: middle; color: red"><span>&#10006;</span></td>
+        <td class="delete" data-id="<?=$id?>" style="text-align: center; cursor: pointer; vertical-align: middle; color: red"><span>&#10006;</span></td>
     </tr>
     <? } ?>
     <tr style="border-top: 4px solid black">
@@ -42,8 +42,8 @@ if ($session['cart']) {
     </tbody>
 </table>
 <div class="modal-buttons" style="display: flex; padding: 15px; justify-content: space-around">
-    <button type="button" class="btn btn-danger"">Очистить корзину</button>
-    <button type=" button" class="btn btn-secondary">Продолжить покупки</button>
+    <button type="button" class="btn btn-danger"onclick="clearCart(event)">Очистить корзину</button>
+    <button type=" button" class="btn btn-secondary btn-close">Продолжить покупки</button>
     <button type="button" class="btn btn-success btn-next">Оформить заказ</button>
 </div>
 
@@ -51,6 +51,6 @@ if ($session['cart']) {
 
 <h3>В вашей корзине ничего нет!</h3>
 
-    <button type=" button" class="btn btn-secondary">Начать покупки</button>
+    <button type=" button" class="btn btn-secondary btn-close">Начать покупки</button>
 
 <? } ?>
